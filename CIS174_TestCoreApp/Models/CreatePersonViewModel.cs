d7b0227b676e686ec3261a3f4fd69a046b681db7
Charwillbro <35745538+Charwillbro@users.noreply.github.com>
2019-09-18 16:55:10 -0500

    Added Validations To all fields

--- CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
+++ CIS174_TestCoreApp/Models/CreatePersonViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel; //This was added to support the Readonly Annotation
+using System.ComponentModel.DataAnnotations;
 
 namespace CIS174_TestCoreApp.Models
 {
@@ -8,12 +9,28 @@ namespace CIS174_TestCoreApp.Models
         [StringLength(25)]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [StringLength(25)]
+        [Display(Name = "Middle Name")]
         public string MiddleName { get; set; }
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [Range(1, 120)]
         public int Age { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Compare("Email")]
+        [Display(Name = "Confirm Email")]
         public string ConfirmEmail { get; set; }
+        [Url]
+        [MinLength(7)]
         public string Website { get; set; }
+        [ReadOnly(true)] //This did not stop me from editing the field, the field info was changed but it DID NOT change on reload
+        //[Editable(false)] //This did not stop me from editing the field, it did make the field info disappear on reload
         public string School { get; set; }
     }
 }
